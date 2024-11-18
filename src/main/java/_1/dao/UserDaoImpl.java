06ac236d1685ae408aea954b2da9ac201864da94
Vladislav <Vladislavmargovich@yandex.ru>
2024-11-19 00:10:21 +0300

    изменения внесены

--- src/main/java/_1/dao/UserDaoImpl.java
+++ src/main/java/_1/dao/UserDaoImpl.java
@@ -8,7 +8,6 @@ import jakarta.persistence.*;
 import java.util.List;
 
 @Repository
-@Transactional
 public class UserDaoImpl implements UserDao {
 
     @PersistenceContext
--- src/main/java/_1/service/UserServiceImpl.java
+++ src/main/java/_1/service/UserServiceImpl.java
@@ -2,6 +2,7 @@ package _1.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import _1.dao.UserDao;
 import _1.model.User;
@@ -16,19 +17,19 @@ public class UserServiceImpl implements UserService {
     private UserDao userDao;
 
     @Override
-    @Transactional
+    @Transactional(propagation = Propagation.REQUIRED)
     public void saveUser(User user) {
         userDao.saveUser(user);
     }
 
     @Override
-    @Transactional
+    @Transactional(propagation = Propagation.REQUIRED)
     public void updateUser(User user) {
         userDao.updateUser(user);
     }
 
     @Override
-    @Transactional
+    @Transactional(propagation = Propagation.REQUIRED)
     public void deleteUser(long id) {
         userDao.deleteUser(id);
     }
